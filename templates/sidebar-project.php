35164329239930d26bf2f699f48cdad32a8a7e36
David Egan <david@carawebs.com>
2016-02-20 20:52:51 +0000

    Add services widget

--- templates/sidebar-project.php
+++ templates/sidebar-project.php
@@ -2,15 +2,16 @@
 /**
  * Sidebar for Projects
  */
+use Carawebs\Carawebs\Display\ProjectSingle;
+
+echo Carawebs\Carawebs\Display\Image::featured_image( get_the_ID(), 'full' );
 ?>
 <div class="project-details">
-  <?= Carawebs\Carawebs\Display\Image::featured_image( get_the_ID(), 'full' );
+  <?php
   echo "<h4>The Client:" . Carawebs\Carawebs\Display\ProjectSingle::client() . "</h4>";
-  echo "</p>";
-  //$our_work = Carawebs\Carawebs\Display\ProjectSingle::our_work();
-  echo Carawebs\Carawebs\Display\ProjectSingle::our_work_ul();
-  echo Carawebs\Carawebs\Display\ProjectSingle::project_features_ul();
-  echo Carawebs\Carawebs\Display\ProjectSingle::project_links_ul();
-  //echo Carawebs\Carawebs\Display\Carousel::the_carousel( 'carousel', 'full', 'basic' ); ?>
+  echo ProjectSingle::our_work_ul();
+  echo ProjectSingle::project_features_ul();
+  echo ProjectSingle::project_links_ul();
+  ?>
 </div>
 <?php
